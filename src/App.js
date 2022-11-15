fb741a98de12bea45d6229edfe205272271de02a
Wiji Fiko Teren <tobellord@gmail.com>
2022-11-15 20:52:56 +0700

    complete: random user interface:

--- src/App.js
+++ src/App.js
@@ -8,11 +8,41 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            quotes: randomQuotes.getRandom(10),
+            quotes: [],
         };
     }
+
+    componentDidMount() {
+        this.setState({ quotes: randomQuotes.getRandom(10) });
+    }
+
+    acak() {
+        const count = window.prompt(
+            "Masukkan jumlah data yang akan kamu tampilkan: "
+        );
+        if (isNaN(count) || count.trim().length < 1) {
+            window.alert("Data yang anda masukkan tidak valid");
+        } else {
+            this.setState({ quotes: randomQuotes.getRandom(parseInt(count)) });
+        }
+    }
+
     render() {
-        return <Header />;
+        return (
+            <>
+                <button onClick={this.acak.bind(this)}>Acak</button>
+                <Header />
+                <div className="items">
+                    {this.state.quotes.map((quote, i) => (
+                        <div className="item" key={i}>
+                            <div className="title">Peribahasa hari ini:</div>
+                            <div className="quote">{quote}</div>
+                            <div className="posted">Posted: Thu jan 2004</div>
+                        </div>
+                    ))}
+                </div>
+            </>
+        );
     }
 }
 
